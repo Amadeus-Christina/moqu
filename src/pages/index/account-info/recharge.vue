313641cc3e0d0cfb23a34f0830a9ff2fc2868118
amadeus <amadeuszzz@126.com>
2019-08-20 14:18:35 +0800

    新增 我的钱包-充值-自定义金额 新增 充值成功提示

--- src/pages/index/account-info/recharge.vue
+++ src/pages/index/account-info/recharge.vue
@@ -4,9 +4,34 @@
       class="wallet-item-group"
       v-for="(item,index) in wallet"
       :key="index" :item="item" :index="index" :activeIndex="activeIndex"
+      @changeAmount="changeAmount"
       @changeActiveIndex="changeActiveIndex"
       />
-    <button class="sure recharge">立即充值</button>
+    <div class="customize" @click="showPopup">
+      <div class="icon"></div>
+      <div class="text">自定义</div>
+    </div>
+    <div class="payment-method">
+      <div class="title">支付方式</div>
+      <div class="wechat-payment">
+        <div class="icon"></div>
+        <div class="text">微信支付</div>
+      </div>
+    </div>
+    <div class="recharge">
+      <button class="sure" @click="showToastSuccess">立即充值</button>
+    </div>
+    <van-popup v-model="show" round>
+      <div class="recharge-pop">
+        <div class="title">充&nbsp;&nbsp;&nbsp;值</div>
+        <div class="close" @click="showPopup"></div>
+        <div class="input-group">
+          <input type="number" class="input" placeholder="请输入充值金额" v-model="amount">
+          <div class="icon">￥</div>
+        </div>
+        <div class="confirm" @click="showToastSuccess">确 定</div>
+      </div>
+    </van-popup>
   </div>
 </template>
 <script>
@@ -19,6 +44,8 @@
     name: '',
     data() {
       return {
+        amount: 50,
+        show: false,
         activeIndex: 0,
         wallet: [
           {
@@ -35,16 +62,12 @@
           },
           {
             diamondNum: 500,
-            price: 450
+            price: 500
           },
           {
             diamondNum: 1000,
-            price: 800
-          },
-          {
-            diamondNum: 2000,
-            price: 1500
-          },
+            price: 1000
+          }
         ]
       }
     },
@@ -55,7 +78,22 @@
       // 切换激活状态
       changeActiveIndex(Index) {
         this.activeIndex = Index
-      }
+      },
+      // 弹出/关闭自定义支付窗口
+      showPopup() {
+        this.show = !this.show;
+      },
+      // 修改充值金额
+      changeAmount(data) {
+        this.amount = data
+      },
+      // 充值成功提示
+      showToastSuccess() {
+        this.$toast.success({
+          message: "充值成功"
+        })
+      },
+
     },
     mounted() {
     },
@@ -73,10 +111,133 @@
   .wallet {
     display: inline-flex;
     flex-wrap: wrap;
-    justify-content: space-around;
+    /*justify-content: space-around;*/
+    align-content: flex-start;
+  }
+  .wallet-item-group{
+    margin-left: 0.3rem;
+  }
+  .customize{
+    width: 2.11rem;
+    height: 1.84rem;
+    border: 1px solid #B5B5B5FF;
+    color: #B5B5B5FF;
+    border-radius: 0.18rem;
+    margin-left: 0.3rem;
+    .icon {
+      width: 0.5rem;
+      height: 1.1rem;
+      background-image: url("/static/images/account-info/Thecustom.png");
+      background-size: 0.5rem 0.5rem;
+      background-position: center center;
+      background-repeat: no-repeat;
+      /*display: inline-block;*/
+      margin: 0 auto;
+    }
+    .text{
+      font-size: 0.28rem;
+      text-align: center;
+    }
+  }
+  .payment-method {
+    font-size: 0.28rem;
+    margin: 0.7rem 0 0.3rem 0.3rem;
+    .title {
+      color: #5C5C5CFF;
+      margin-bottom: 0.3rem;
+    }
+    .wechat-payment {
+      width: 2.11rem;
+      height: 1.05rem;
+      border: 1px solid #FFC824FF;
+      border-radius: 0.18rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        margin-right: 0.2rem;
+        display: inline-block;
+        width: 0.4rem;
+        height: 0.4rem;
+        background-image: url("/static/images/account-info/weixinzhifu.png");
+        background-size: 0.4rem 0.4rem;
+        background-position: center center;
+        background-repeat: no-repeat;
+      }
+      .text{
+        display: inline-block;
+        font-size: 0.28rem;
+        color: #FFC824FF;
+      }
+    }
   }
   .recharge{
+    margin: 0 auto;
     position: absolute;
     bottom: 1.4rem;
+    left: 0.8rem;
+  }
+  .recharge-pop{
+    width: 5.4rem;
+    height: 3.05rem;
+    background: #ffffff;
+    .title{
+      height: 0.95rem;
+      border-bottom:1px solid #F5F5F5;
+      color: #424242;
+      font-size: 0.32rem;
+      text-align: center;
+      line-height: 0.95rem;
+      position: relative;
+    }
+    .close {
+      width: 0.27rem;
+      height: 0.27rem;
+      background-image: url("/static/images/account-info/Shutdown.png");
+      background-size: 0.27rem 0.27rem;
+      position: absolute;
+      top: 0.3rem;
+      right: 0.2rem;
+    }
+    .input-group {
+      height: 1.2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .input {
+        position: relative;
+        display: inline-block;
+        width: 4.8rem;
+        height: 0.6rem;
+        padding-left: 0.1rem;
+        font-size: 0.28rem;
+        border: 1px solid #B5B5B5;
+        border-radius: 0.18rem;
+      }
+      .icon {
+        position: absolute;
+        right: 0.4rem;
+        display: inline-block;
+        width: 0.72rem;
+        height: 0.5rem;
+        border-left: 1px solid #F5F5F5;
+        font-size: 0.36rem;
+        font-weight: 600;
+        color: #424242;
+        text-align: center;
+        align-items: center;
+      }
+    }
+    .confirm {
+      width: 2rem;
+      height: 0.6rem;
+      background-color: #23D2CD;
+      border-radius: 0.3rem;
+      color: #FFFFFF;
+      font-size: 0.28rem;
+      text-align: center;
+      line-height: 0.6rem;
+      margin: 0 auto;
+    }
   }
 </style>
