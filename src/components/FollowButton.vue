27a405093d5b99e47ca21a0d7547a0842c308734
amadeus <amadeuszzz@126.com>
2019-08-27 18:02:27 +0800

    关注页面 视频认证页面

--- src/components/FollowButton.vue
+++ src/components/FollowButton.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="follow-button" :class="{ 'green' :  (showView == 'follower') && (following == false)}">
-    <span v-if="showView === 'follow'">
-      <span v-if = 'isFollower === true'>互相关注</span>
-      <span v-else>取消关注</span>
-    </span>
-    <span v-else>
-      <span v-if = 'following === true'>互相关注</span>
-      <span v-else>关注</span>
+    <span v-if = 'following === true' @click="cancelFollow">取消关注</span>
+    <span v-else @click="addFollow">
+        <span class="plus">+</span>
+        <span class="text-not-follow">关注</span>
     </span>
   </div>
 </template>
 <script>
+import {intercommunication, addAttention, cancelAttention} from '@/api/my/index.js'
 export default {
   components: {},
   mixins: [],
   name: '',
   data () {
-    return {}
+    return {
+      following: true
+    }
   },
-  props: ["showView","following","isFollower"],
+  props: ["showView",'itemUserId'],
   computed: {},
   watch: {},
   methods: {
+    // 获取关注状态
+    getStatus () {
+      intercommunication(this.$store.state.userInfo.userId, this.itemUserId).then(res => {
+        if (res.code ==200 ){
+          if (res.data == 0){
+            this.following = true
+          } else {
+            this.following = false
+          }
+        }
+      })
+    },
+    //添加关注
+    addFollow(){
+      addAttention(this.$store.state.userInfo.userId, this.itemUserId).then(res => {
+        if (res.code == 200) {
+          console.log('添加关注成功')
+          this.following = !this.following
+        } else {
+          this.$toast(res.msg)
+        }
+      })
+    },
+    // 取消关注
+    cancelFollow(){
+      cancelAttention(this.$store.state.userInfo.userId, this.itemUserId).then(res => {
+        if (res.code == 200) {
+          console.log('取消关注成功')
+          this.following = !this.following
+        } else {
+          this.$toast(res.msg)
+        }
+      })
+    },
   },
   mounted () {
+    this.getStatus()
   },
   created () {},
   filters: {},
@@ -44,6 +79,22 @@ export default {
     font-size: 0.24rem;
     text-align: center;
     line-height: 0.5rem;
+    position: relative;
+
+    .plus {
+      font-size: 0.5rem;
+      font-weight: 500;
+      color: #ffffff;
+      position: absolute;
+      top: -0.04rem;
+      left: 0.1rem;
+    }
+    .text-not-follow {
+      font-size: 0.24rem;
+      color: #FFFFFF;
+      position: absolute;
+      right: 0.2rem;
+    }
   }
   .green {
     background: #23D2CDFF;
--- src/pages/index/follow-list/index.vue
+++ src/pages/index/follow-list/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="">
     <follow-title @show="show"/>
-    <div v-if="followUserInfo !== undefined && followUserInfo.length > 0 ">
-      <div v-if="showView ===  'follow'" class="follow-group" v-for="(item,index) in followUserInfo" :key="index">
+    <div v-if="attentionList !== undefined && attentionList.length > 0 ">
+      <div v-if="showView ===  'follow'" class="follow-group" v-for="(item,index) in attentionList" :key="index">
         <follow-item :followUserInfo="item" :showView="showView"/>
       </div>
     </div>
-    <div v-if="showView ===  'follow' && (followUserInfo == undefined || followUserInfo.length <= 0)">
+    <div v-if="showView ===  'follow' && (attentionList == undefined || attentionList.length <= 0)">
       <blank-data :message="'目前还没有关注'"/>
     </div>
-    <div v-if="followerUserInfo !== undefined && followerUserInfo.length > 0 ">
-      <div v-if="showView ===  'follower'" class="follow-group" v-for="(item,index) in followerUserInfo" :key="index">
+    <div v-if="fansList !== undefined && fansList.length > 0 ">
+      <div v-if="showView ===  'follower'" class="follow-group" v-for="(item,index) in fansList" :key="index">
         <follow-item :followUserInfo="item" :showView="showView"/>
       </div>
     </div>
-    <div v-if="showView ===  'follower' && (followerUserInfo == undefined || followerUserInfo.length <= 0)">
+    <div v-if="showView ===  'follower' && (fansList == undefined || fansList.length <= 0)">
       <blank-data :message="'目前还没有粉丝'"/>
     </div>
   </div>
@@ -23,6 +23,7 @@
 import followTitle from '@/components/FollowTitle'
 import followItem from '@/components/FollowItem'
 import blankData from '@/components/BlankData'
+import {myAttention, myFans} from '@/api/my/follow.js'
 export default {
   components: {
     followTitle,
@@ -34,93 +35,93 @@ export default {
   data () {
     return {
       showView: 'follow',
-      followUserInfo: [
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '山鬼',
-          anonymousUsername: '九月',
-          gender: '男',
-          age: 20,
-          following: true,
-          isFollower: true
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈龙',
-          anonymousUsername: '敖烈',
-          gender: '女',
-          age: 21,
-          following: true,
-          isFollower: false
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈龙',
-          anonymousUsername: '敖烈',
-          gender: '女',
-          age: 22,
-          following: true,
-          isFollower: true,
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈龙',
-          anonymousUsername: '敖烈',
-          gender: '男',
-          age: 20,
-          following: true,
-          isFollower: false,
-        }
-      ],
-      followerUserInfo: [
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '山鬼',
-          anonymousUsername: '九月',
-          gender: '男',
-          age: 24,
-          following: true,
-          isFollower: true
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈龙',
-          anonymousUsername: '敖',
-          gender: '女',
-          age: 27,
-          following: false,
-          isFollower: true
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈',
-          anonymousUsername: '敖烈',
-          gender: '女',
-          age: 27,
-          following: true,
-          isFollower: true,
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈龙',
-          anonymousUsername: '敖烈',
-          gender: '男',
-          age: 20,
-          following: true,
-          isFollower: true,
-        },
-        {
-          profile: '/static/images/mock/03.jpg',
-          username: '烈烈龙',
-          anonymousUsername: '敖烈',
-          gender: '男',
-          age: 20,
-          following: false,
-          isFollower: true,
-        }
-      ],
-      blankfollowUserInfo: [],
-      blankfollowerUserInfo: []
+      // followUserInfo: [
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '山鬼',
+      //     anonymousUsername: '九月',
+      //     gender: '男',
+      //     age: 20,
+      //     following: true,
+      //     isFollower: true
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈龙',
+      //     anonymousUsername: '敖烈',
+      //     gender: '女',
+      //     age: 21,
+      //     following: true,
+      //     isFollower: false
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈龙',
+      //     anonymousUsername: '敖烈',
+      //     gender: '女',
+      //     age: 22,
+      //     following: true,
+      //     isFollower: true,
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈龙',
+      //     anonymousUsername: '敖烈',
+      //     gender: '男',
+      //     age: 20,
+      //     following: true,
+      //     isFollower: false,
+      //   }
+      // ],
+      // followerUserInfo: [
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '山鬼',
+      //     anonymousUsername: '九月',
+      //     gender: '男',
+      //     age: 24,
+      //     following: true,
+      //     isFollower: true
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈龙',
+      //     anonymousUsername: '敖',
+      //     gender: '女',
+      //     age: 27,
+      //     following: false,
+      //     isFollower: true
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈',
+      //     anonymousUsername: '敖烈',
+      //     gender: '女',
+      //     age: 27,
+      //     following: true,
+      //     isFollower: true,
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈龙',
+      //     anonymousUsername: '敖烈',
+      //     gender: '男',
+      //     age: 20,
+      //     following: true,
+      //     isFollower: true,
+      //   },
+      //   {
+      //     profile: '/static/images/mock/03.jpg',
+      //     username: '烈烈龙',
+      //     anonymousUsername: '敖烈',
+      //     gender: '男',
+      //     age: 20,
+      //     following: false,
+      //     isFollower: true,
+      //   }
+      // ],
+      attentionList: [],
+      fansList:[]
     }
   },
   props: {},
@@ -130,9 +131,34 @@ export default {
     // 更改展示的页面
     show (data) {
       this.showView = data
+    },
+    // 获取关注列表
+    getAttention() {
+      myAttention(this.$store.state.userInfo.userId).then(res => {
+        if (res.code == 200){
+          this.attentionList = res.data
+          console.log('Attention',res.data)
+        } else {
+          console.log(res.msg)
+        }
+      })
+    },
+    // 获取粉丝列表
+    getFans() {
+      myFans(this.$store.state.userInfo.userId).then(res => {
+        if (res.code == 200){
+          this.fansList = res.data
+          console.log("fans",res.data)
+        } else {
+          console.log(res.msg)
+        }
+      })
     }
   },
-  mounted () {},
+  mounted () {
+    this.getAttention()
+    this.getFans()
+  },
   created () {},
   filters: {},
   directives: {},
